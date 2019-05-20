cd90952b9c2eacc08faf85bd9004e9fcac251a34
anusorns <advancescad@gmail.com>
2019-05-20 10:41:02 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -135,7 +135,7 @@ echo "OK";
 else if($message == "ORDER"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "อย่าทิ้งกันไป";
+        $arrayPostData['messages'][0]['button'] = "อย่าทิ้งกันไป";
         $arrayPostData['messages'][1]['type'] = "sticker";
         $arrayPostData['messages'][1]['packageId'] = "1";
         $arrayPostData['messages'][1]['stickerId'] = "131";
